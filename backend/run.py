71d4f8b63c64f1637c1647916d08eff5b8e9c5ea
Nivin24 <nivinbennyvpz@gmail.com>
2025-11-18 13:58:46 +0530

    updated run.py for production

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,10 @@
 from app import create_app
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    env = os.getenv("FLASK_ENV", "development")
+    debug = env == "development"
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", debug=debug, port=port)
